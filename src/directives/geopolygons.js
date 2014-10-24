1b9b1195462ce5e504b021142cdb7f024e0dd52b
Will Leahy <willl@Jonathans-iMac.local>
2014-10-24 15:05:06 -0700

    improvements to geopolygons. Added issues section to examples

--- src/directives/geopolygons.js
+++ src/directives/geopolygons.js
@@ -21,6 +21,8 @@ angular.module('ng-data-map')
 
           var polygons = [];
 
+          var options = $scope.options ? $scope.options : function() { return {} };
+
           $scope.$watch(function() {
             return $scope.options;
           }, function() {
@@ -33,42 +35,44 @@ angular.module('ng-data-map')
 
             angular.forEach(data.features, function(p, i) {
 
-              // Express each coordinate pair as a google maps object
-              for (var j = 0; j < p.geometry.coordinates.length; j++) {
-                var coords = p.geometry.coordinates[j];
-                for (var k = 0; k < coords.length; k++) {
+              for(var j = 0; j < p.geometry.coordinates.length; j++) {
+
+                var coords = p.geometry.coordinates[j]
+
+                for (var k = 0; k < p.geometry.coordinates[j].length; k++) {
                   coords[k] = new google.maps.LatLng(coords[k][1], coords[k][0]);
                 }
-              }
 
-              var polygon = new google.maps.Polygon({
-                paths: p.geometry.coordinates
-              });
-
-              polygon.setOptions($scope.options(p, MapObjects));
-
-              polygon.setMap(map);
-
-              // Assign properties to marker
-              polygon.properties = p.properties;
-
-              // Assign geometry to marker
-              polygon.geometry = p.geometry;
-
-              // Helper function so multimarkers' API matches data layer
-              polygon.getProperty = function(p) {
-                return this.properties[p];
-              };
-
-              polygons.push(polygon);
-
-              // For each event, add a listener. Also provides access to the map and parent scope
-              // For some reason, the val function requires "this" instead of "polygon"
-              angular.forEach($scope.events, function(val, key) {
-                google.maps.event.addListener(polygon, key, function(e) {
-                  val(e, this, MapObjects);
+                var polygon = new google.maps.Polygon({
+                  paths: coords
                 });
-              });
+
+                polygon.setOptions(options(p, MapObjects));
+
+                polygon.setMap(map);
+
+                // Assign properties to marker
+                polygon.properties = p.properties;
+
+                // Assign geometry to marker
+                polygon.geometry = p.geometry;
+
+                // Helper function so multimarkers' API matches data layer
+                polygon.getProperty = function(p) {
+                  return this.properties[p];
+                };
+
+                polygons.push(polygon);
+
+                // For each event, add a listener. Also provides access to the map and parent scope
+                // For some reason, the val function requires "this" instead of "polygon"
+                angular.forEach($scope.events, function(val, key) {
+                  google.maps.event.addListener(polygon, key, function(e) {
+                    val(e, this, MapObjects);
+                  });
+                });
+
+              };
 
 
             });
