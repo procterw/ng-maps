37d5362aa6ab192d8cdbda765f6b86c1078f5d89
Joel Dubowy <jdubowy@gmail.com>
2015-08-17 10:09:24 -0700

    Create new build to include kml diretive

--- dist/ng-maps.js
+++ dist/ng-maps.js
@@ -460,6 +460,63 @@ angular.module('ngMaps')
       }
     };
   }]);;angular.module('ngMaps')
+  .directive('kml', [function() {
+    return {
+      restrict: 'E',
+      scope: {
+        url: '=',
+        visible: '='
+      },
+      require: '^map',
+      link: function($scope, $element, $attrs, parent) {
+
+        $scope.$watch(function() {
+          parent.getMap();
+        }, function() {
+
+          var map = parent.getMap();
+
+          var delete_kml = function() {
+            if (kml) {
+              kml.setMap(null);
+              kml = null;
+            }
+          };
+
+          var new_kml = function() {
+            delete_kml();
+
+            var kml = new google.maps.KmlLayer({
+                url: $scope.url,
+                map: map
+            });
+
+            if ($scope.visible !== false) {
+              kml.setMap(map);
+            } else {
+              kml.setMap(null);
+            }
+
+            return kml;
+          };
+
+          var kml = new_kml();
+
+          $scope.$watch('url + bounds', function() {
+            kml = new_kml();
+          });
+
+          $scope.$watch('visible', function() {
+            if ($scope.visible !== false) {
+              kml.setMap(map);
+            } else {
+              kml.setMap(null);
+            }
+          });
+        });
+      }
+    };
+  }]);;angular.module('ngMaps')
   .directive('map', [function() {
     return {
       restrict: 'AE',
