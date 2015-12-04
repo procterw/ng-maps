6fcf77e1983eda6c7ba74184b8bad6776cbe3a5b
Rosana <totemika@gmail.com>
2015-12-04 12:16:24 +0100

    Remove unused comments

--- src/directives/textLabels.js
+++ src/directives/textLabels.js
@@ -84,37 +84,6 @@ angular.module('ngMaps')
 
           });
 
-          // opts.position = currentPosition();
-          // opts.map = map;
-
-          // var marker = new google.maps.Marker(opts);
-
-          // // For each event, add a listener. Also provides access to the map and parent scope
-          // angular.forEach($scope.events, function(val, key) {
-          //   google.maps.event.addListener(marker, key, function(e) {
-          //     val(e, marker, map);
-          //   });
-          // });
-
-          // // Watch for changes in position and move marker when they happen
-          // $scope.$watch('[position, lat, lng]', function() {
-          //   marker.setPosition(currentPosition());
-          // }, true);
-
-          // // When the marker is dragged, update the scope with its new position
-          // google.maps.event.addListener(marker, "drag", function() {
-          //   $scope.$apply(function() {
-          //     var lat = round(marker.getPosition().lat());
-          //     var lng = round(marker.getPosition().lng());
-          //     if ($scope.position) {
-          //       $scope.position = [lat, lng];
-          //     } else if ($scope.lat && $scope.lng) {
-          //       $scope.lat = lat;
-          //       $scope.lng = lng;
-          //     }
-          //   });
-          // });
-
         });
       }
     };
